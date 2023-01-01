8159ab5f83722df95d3dba1ce082e87a66c60dd0
Ganesh Tiwari <ganesht049@gmail.com>
2023-01-01 20:02:27 +0530

    added sidenav initial component

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,24 +1,84 @@
 import React from 'react'
+
+import Image from 'next/image'
+import { SocialIcon } from "react-social-icons";
+
 import styles from "../styles/components/SideNav.module.css";
 
 export default function SideNav() {
   return (
     <nav className={`${styles.root}`}>
-        <div className=''>
-            <img 
-                src="chainlink-blue-logo.svg" 
-                alt="Chainlink logo" 
-                className={`${styles.chainlinkLogo}`} />
-            
-            <div>
-                <h1>
-                    Chainlink
-                </h1>
+        <div className={`${styles.logoHolder}`}>
+            <div className='flex justify-center items-center'>
+                <img 
+                    src="chainlink-blue-logo.svg" 
+                    alt="Chainlink logo" 
+                    className={`${styles.chainlinkLogo}`} />
+                
+                <div>
+                    <h1 className='text-custom_blue text-3xl font-medium m-0 p-0'>
+                        Chainlink
+                    </h1>
 
-                <span>
-                    Mumbai Community
-                </span>
+                    <span className='uppercase text-xs m-0 p-0'>
+                        Mumbai Community
+                    </span>
+                </div>
             </div>
+        </div>
+
+        <ul className={`${styles.navItems}`}>
+            <li>
+                <span className="material-symbols-outlined">home</span>
+                Home
+            </li>
+            <li>
+                <span className="material-symbols-outlined">calendar_month</span>
+                Upcoming Events
+            </li>
+            <li>
+                <span className="material-symbols-outlined">star_half</span>
+                Past Events
+            </li>
+            <li>
+                <span className="material-symbols-outlined">description</span>
+                About Chainlink
+            </li>
+            <li>
+                <span className="material-symbols-outlined">diversity_4</span>
+                Our Team
+            </li>
+            <li>
+                <span className="material-symbols-outlined">join_right</span>
+                Join us
+            </li>
+        </ul>
+
+        <div className={`${styles.social_icons_container}`}>
+            <span className='uppercase text-gray-600 text-sm'>
+                Connect with us here
+            </span>
+            <ul className='flex gap-2'>
+                <li>
+                    <Image 
+                        src={"/social-icons/twitter-dark-logo.png"}
+                        width="24"
+                        height={"24"}
+                        alt="Join us on Twitter icon"/>
+                </li>
+                <li>
+                    <Image 
+                        src={"/social-icons/whatsapp-logo.svg"}
+                        width="24"
+                        height={"24"}
+                        alt="Join us on Whatsapp icon"/>
+                </li>
+                <li>
+                    <span className="material-symbols-outlined">
+                        join
+                    </span>
+                </li>
+            </ul>
         </div>
     </nav>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom_blue: '#375BD2',
+      }
+    },
   },
   plugins: [],
 }
